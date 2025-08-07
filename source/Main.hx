83f3032a9c0318518f1d4be885b1705510710c28
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-08 01:19:08 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -14,6 +14,7 @@ import openfl.events.KeyboardEvent;
 import lime.system.System as LimeSystem;
 #if mobile
 import mobile.backend.MobileScaleMode;
+import mobile.object.MobileControls;
 #end
 #if linux
 import lime.graphics.Image;
