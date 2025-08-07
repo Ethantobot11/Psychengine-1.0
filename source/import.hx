32e8cac89cdbe0d25bd1993997f1a1f3d9ee5ee0
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-08 01:01:45 +0200

    Useless with astc?

--- source/import.hx
+++ source/import.hx
@@ -15,7 +15,6 @@ import backend.Achievements;
 #end
 
 //Mobile Controls
-import mobile.objects.MobileControls;
 import mobile.objects.Hitbox;
 import mobile.objects.TouchPad;
 import mobile.input.MobileInputID;
