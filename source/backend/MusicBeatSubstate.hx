d7e548a825c1690befdd3b3561e429a8ea879d66
Ethantobot <156458071+Ethantobot11@users.noreply.github.com>
2025-08-08 01:22:08 +0200

    Update MusicBeatSubstate.hx

--- source/backend/MusicBeatSubstate.hx
+++ source/backend/MusicBeatSubstate.hx
@@ -29,7 +29,7 @@ class MusicBeatSubstate extends FlxSubState
 		return Controls.instance;
 
 	public var touchPad:TouchPad;
-	public var mobileControls:TouchButton;
+	public var mobileControls:MobileControls;
 
 	public function addMobileControls(defaultDrawTarget:Bool = true):Void
 		{
